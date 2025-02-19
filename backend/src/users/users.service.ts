7918c18a24bec7fab40818956c414b95606840b6
meow <69355934+MeowKyWay@users.noreply.github.com>
2025-02-19 19:04:49 +0700

    Add user module/service

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+export type User = {
+  id: number;
+  email: string;
+  name: string | null;
+  password: string;
+  hashedRefreshToken: string | null;
+};
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      return undefined;
+    }
+    return {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      password: user.password,
+      hashedRefreshToken: user.hashedRefreshToken,
+    };
+  }
+}
