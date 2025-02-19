5c6b1f8984a33e2ed6811528811d71560331d0df
meow <69355934+MeowKyWay@users.noreply.github.com>
2025-02-19 20:02:55 +0700

    Added package passport-jwt

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -15,7 +15,6 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   @Post('signin')
   login(@Request() req) {
-    console.log('AuthController.login()');
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
     return req.user;
   }
