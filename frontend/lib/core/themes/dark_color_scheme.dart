4d3cfc8fc9fbbb31a0192269d17ba42c5bc05139
meow <69355934+MeowKyWay@users.noreply.github.com>
2025-02-21 22:26:25 +0700

    Update dark_color_scheme.dart

--- frontend/lib/core/themes/dark_color_scheme.dart
+++ frontend/lib/core/themes/dark_color_scheme.dart
@@ -4,13 +4,14 @@ final darkColorScheme = ColorScheme(
   brightness: Brightness.dark,
 
   primary: Color.fromARGB(255, 39, 37, 34), //Appbar Navbar etc
-  onPrimary: Colors.white, //Text on Appbar Navbar etc
+  onPrimary: Colors.white, //primary text color
   secondary: Color.fromARGB(255, 139, 168, 89), //Buttons etc
-  onSecondary: Colors.white, //Text on Buttons etc
+  onSecondary: Color.fromARGB(255, 190, 189, 188), //secondary text color
   tertiary: Color.fromARGB(255, 66, 62, 59), //Textinput fill etc
   onTertiary: Color.fromARGB(255, 140, 139, 137), //Textfield hint color
 
   outline: Colors.white,
+  outlineVariant: Color.fromARGB(255, 93, 93, 89),
 
   error: Color.fromARGB(255, 223, 75, 75),
   onError: Colors.white,
