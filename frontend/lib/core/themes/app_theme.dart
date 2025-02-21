42a699eb84e2319ed1fe151cbbcddb0ed4208b8d
meow <69355934+MeowKyWay@users.noreply.github.com>
2025-02-21 20:48:47 +0700

    More theming

--- frontend/lib/core/themes/app_theme.dart
+++ frontend/lib/core/themes/app_theme.dart
@@ -10,6 +10,10 @@ class AppTheme {
     return ThemeData(
       brightness: colorScheme.brightness,
       primaryColor: colorScheme.primary,
+      textSelectionTheme: TextSelectionThemeData(
+        cursorColor: Colors.white,
+        selectionColor: Colors.white30,
+      ),
       scaffoldBackgroundColor: colorScheme.surface,
       appBarTheme: AppBarTheme(
         backgroundColor: colorScheme.primary,
@@ -33,6 +37,37 @@ class AppTheme {
           ),
           foregroundColor: WidgetStateProperty.all(colorScheme.onSecondary),
         ),
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: WidgetStateProperty.all(colorScheme.onPrimary),
+          backgroundColor: WidgetStateProperty.all(Colors.transparent),
+          splashFactory: NoSplash.splashFactory,
+          overlayColor: WidgetStateProperty.all(Colors.transparent),
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        filled: true,
+        fillColor: colorScheme.tertiary,
+        hoverColor: Colors.transparent,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(color: colorScheme.outline),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(color: colorScheme.error),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(color: colorScheme.error),
+        ),
+        hintStyle: TextStyle(color: colorScheme.onTertiary),
+        prefixIconColor: colorScheme.onTertiary,
       ),
     );
   }
--- frontend/lib/core/themes/dark_color_scheme.dart
+++ frontend/lib/core/themes/dark_color_scheme.dart
@@ -3,14 +3,19 @@ import 'package:flutter/material.dart';
 final darkColorScheme = ColorScheme(
   brightness: Brightness.dark,
 
-  primary: Color.fromARGB(255, 39, 37, 34),
-  onPrimary: Colors.white,
-  secondary: Color.fromARGB(255, 139, 168, 89),
-  onSecondary: Colors.white,
+  primary: Color.fromARGB(255, 39, 37, 34), //Appbar Navbar etc
+  onPrimary: Colors.white, //Text on Appbar Navbar etc
+  secondary: Color.fromARGB(255, 139, 168, 89), //Buttons etc
+  onSecondary: Colors.white, //Text on Buttons etc
+  tertiary: Color.fromARGB(255, 66, 62, 59), //Textinput fill etc
+  onTertiary: Color.fromARGB(255, 140, 139, 137), //Textfield hint color
+
+  outline: Colors.white,
 
   error: Color.fromARGB(255, 223, 75, 75),
   onError: Colors.white,
-  surface: Color.fromARGB(255, 48, 46, 43),
+
+  surface: Color.fromARGB(255, 50, 46, 43),
   onSurface: Colors.white,
 );
 
