02168895b98fafb0f5039eec0b0e74494cf55a05
meow <69355934+MeowKyWay@users.noreply.github.com>
2025-02-22 11:58:56 +0700

    Update build_context_extension.dart
    Add push for easy Navigator.push

--- frontend/lib/core/extensions/build_context_extension.dart
+++ frontend/lib/core/extensions/build_context_extension.dart
@@ -4,4 +4,8 @@ extension BuildContextExtension on BuildContext {
   ThemeData get theme => Theme.of(this);
   TextTheme get textTheme => theme.textTheme;
   ColorScheme get colorScheme => theme.colorScheme;
+
+  void push(Widget page) {
+    Navigator.of(this).push(MaterialPageRoute(builder: (_) => page));
+  }
 }
