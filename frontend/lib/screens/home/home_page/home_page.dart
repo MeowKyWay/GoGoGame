a30ba6933f8118e2df6fdcecfb61184f8cd233c3
meow <69355934+MeowKyWay@users.noreply.github.com>
2025-02-24 20:06:20 +0700

    Update home_page.dart
    add function to play button

--- frontend/lib/screens/home/home_page/home_page.dart
+++ frontend/lib/screens/home/home_page/home_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gogogame_frontend/core/extensions/build_context_extension.dart';
+import 'package:gogogame_frontend/screens/home/home_screen.dart';
+import 'package:gogogame_frontend/screens/play/play_screen.dart';
 import 'package:gogogame_frontend/widget/text_divider.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends ConsumerWidget implements AppHomePage {
   const HomePage({super.key});
+
+  @override
+  Widget get title => const Text('GoGoGame');
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -25,7 +31,7 @@ class HomePage extends ConsumerWidget {
                   width: double.infinity,
                   child: FilledButton(
                     onPressed: () {
-                      throw UnimplementedError();
+                      context.push(PlayScreen());
                     },
                     child: Text('Play'),
                   ),
