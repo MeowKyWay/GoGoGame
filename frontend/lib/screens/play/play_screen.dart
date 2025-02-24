8dd205bc0f658ea7353954fb3e043749e9a20e86
meow <69355934+MeowKyWay@users.noreply.github.com>
2025-02-25 01:45:21 +0700

    Update play_screen.dart

--- frontend/lib/screens/play/play_screen.dart
+++ frontend/lib/screens/play/play_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:gogogame_frontend/core/constants/board_size_type.dart';
 import 'package:gogogame_frontend/core/constants/time_control.dart';
+import 'package:gogogame_frontend/widget/select_button.dart';
 
 class PlayScreen extends StatefulWidget {
   const PlayScreen({super.key});
@@ -17,43 +19,83 @@ class _PlayScreenState extends State<PlayScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Play')),
-      body: Column(
-        children: [
-          DropdownButton<BoardSize>(
-            items:
-                BoardSize.boardSizes
-                    .map(
-                      (e) => DropdownMenuItem<BoardSize>(
-                        value: e,
-                        child: Text(e.toString()),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Flexible(flex: 1, child: Center(child: Text('logo'))),
+            Flexible(
+              flex: 1,
+              child: Column(
+                spacing: 8,
+                children: [
+                  Gap(56),
+                  Row(
+                    spacing: 8,
+                    children: [
+                      Flexible(
+                        flex: 1,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text('Board size'),
+                            SizedBox(
+                              height: 56,
+                              child: SelectButton<BoardSize>(
+                                items: BoardSize.boardSizes,
+                                prefixIcon: const Icon(Icons.grid_on),
+                                onChanged: (value) {
+                                  setState(() {
+                                    _boardSize = value!;
+                                  });
+                                },
+                                value: _boardSize,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    )
-                    .toList(),
-            onChanged: (value) {
-              setState(() {
-                _boardSize = value!;
-              });
-            },
-            value: _boardSize,
-          ),
-          DropdownButton<TimeControl>(
-            items:
-                TimeControl.timeControls
-                    .map(
-                      (e) => DropdownMenuItem<TimeControl>(
-                        value: e,
-                        child: Text(e.toString()),
+                      Flexible(
+                        flex: 1,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text('Time control'),
+                            SizedBox(
+                              height: 56,
+                              child: SelectButton<TimeControl>(
+                                items: TimeControl.timeControls,
+                                prefixIcon: const Icon(Icons.timer),
+                                onChanged: (value) {
+                                  setState(() {
+                                    _timeControl = value!;
+                                  });
+                                },
+                                value: _timeControl,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    )
-                    .toList(),
-            onChanged: (value) {
-              setState(() {
-                _timeControl = value!;
-              });
-            },
-            value: _timeControl,
-          ),
-        ],
+                    ],
+                  ),
+                  SizedBox(
+                    height: 56,
+                    width: double.infinity,
+                    child: FilledButton(
+                      onPressed: () {
+                        throw UnimplementedError();
+                      },
+                      child: const Text('Play'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Gap(56),
+          ],
+        ),
       ),
     );
   }
