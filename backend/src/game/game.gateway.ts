d3a78ff7164702a2d79267fb5cc413ea0fae2039
meow <69355934+MeowKyWay@users.noreply.github.com>
2025-02-25 12:59:33 +0700

    Update game.gateway.ts

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -5,12 +5,16 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   WebSocketServer,
+  MessageBody,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { MatchmakingService } from './match-making/match-making.service';
-import { ConnectedPlayer } from './types/connected-player.type';
+import { ConnectedPlayer } from './types/player.type';
 import { AuthService } from 'src/auth/auth.service';
 import { PublicUser } from 'src/users/users.service';
+import { JoinQueueDto } from './dto/join-queue.dto';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
 // import { GameService } from './game.service';
 
 @WebSocketGateway(3001)
@@ -49,11 +53,34 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   handleDisconnect(client: Socket) {
     console.log(`Client disconnected: ${client.id}`);
+    this.matchmakingService.removeFromQueue(this.activeSockets[client.id]);
     delete this.activeSockets[client.id];
   }
 
   @SubscribeMessage('joinQueue')
-  handleJoinQueue(@ConnectedSocket() client: Socket) {
-    this.matchmakingService.addToQueue(this.activeSockets[client.id]);
+  async handleJoinQueue(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() joinQueueDto: JoinQueueDto,
+  ) {
+    const dtoInstance = plainToInstance(JoinQueueDto, joinQueueDto);
+    const errors = await validate(dtoInstance);
+
+    if (errors.length > 0) {
+      console.log(`Validation failed:`, errors);
+      client.emit('validationError', {
+        message: errors.map((e) => e.toString()),
+      });
+      return;
+    }
+
+    this.matchmakingService.addToQueue({
+      connectedPlayer: this.activeSockets[client.id],
+      ...joinQueueDto,
+    });
+  }
+
+  @SubscribeMessage('leaveQueue')
+  handleLeaveQueue(@ConnectedSocket() client: Socket) {
+    this.matchmakingService.removeFromQueue(this.activeSockets[client.id]);
   }
 }
