88043ba1585bc98a5354a97f377978d524f84eb5
meow <69355934+MeowKyWay@users.noreply.github.com>
2025-02-27 10:28:02 +0700

    Update more_page.dart
    Add logout button

--- frontend/lib/screens/home/more_page/more_page.dart
+++ frontend/lib/screens/home/more_page/more_page.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gogogame_frontend/core/services/auth/auth_service_provider.dart';
 import 'package:gogogame_frontend/screens/home/home_screen.dart';
 
-class MorePage extends StatelessWidget implements AppHomePage {
+class MorePage extends ConsumerWidget implements AppHomePage {
   const MorePage({Key? key});
 
   @override
   Widget get title => const Text('More');
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text('More'));
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Center(
+      child: FilledButton(
+        onPressed: () {
+          ref.read(authState.notifier).logout();
+        },
+        child: Text('Logout'),
+      ),
+    );
   }
 }
