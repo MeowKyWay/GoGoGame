af6341d6f844e9039603c894ad385214484fff80
meow <69355934+MeowKyWay@users.noreply.github.com>
2025-03-15 16:38:39 +0700

    Update game_theme.dart
    Remove unuse code

--- frontend/lib/core/themes/game_theme.dart
+++ frontend/lib/core/themes/game_theme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gogogame_frontend/core/types/game_type.dart';
 
 final gameThemeProvider = StateNotifierProvider<GameThemeNotifier, GameTheme>(
   (ref) => GameThemeNotifier(),
@@ -11,9 +12,8 @@ class GameThemeNotifier extends StateNotifier<GameTheme> {
         const GameTheme(
           boardColor: Color(0xFFD7A96B), // Light wood
           lineColor: Colors.black,
-          starPointColor: Colors.black,
-          blackStoneColor: Colors.black,
-          whiteStoneColor: Colors.white,
+          blackDiskColor: Colors.black,
+          whiteDiskColor: Colors.white,
           boardImage: null, // Default is no image
         ),
       );
@@ -26,34 +26,34 @@ class GameThemeNotifier extends StateNotifier<GameTheme> {
 class GameTheme {
   final Color? boardColor;
   final Color lineColor;
-  final Color starPointColor;
-  final Color blackStoneColor;
-  final Color whiteStoneColor;
+  final Color blackDiskColor;
+  final Color whiteDiskColor;
   final String? boardImage; // New field for image background
 
   const GameTheme({
     this.boardColor, // Can be null if using an image
     required this.lineColor,
-    required this.starPointColor,
-    required this.blackStoneColor,
-    required this.whiteStoneColor,
+    required this.blackDiskColor,
+    required this.whiteDiskColor,
     this.boardImage,
   });
+
+  Color getDiskColor(DiskColor color) {
+    return color == DiskColor.black ? blackDiskColor : whiteDiskColor;
+  }
 
   GameTheme copyWith({
     Color? boardColor,
     Color? lineColor,
-    Color? starPointColor,
-    Color? blackStoneColor,
-    Color? whiteStoneColor,
+    Color? blackDiskColor,
+    Color? whiteDiskColor,
     String? boardImage,
   }) {
     return GameTheme(
       boardColor: boardColor ?? this.boardColor,
       lineColor: lineColor ?? this.lineColor,
-      starPointColor: starPointColor ?? this.starPointColor,
-      blackStoneColor: blackStoneColor ?? this.blackStoneColor,
-      whiteStoneColor: whiteStoneColor ?? this.whiteStoneColor,
+      blackDiskColor: blackDiskColor ?? this.blackDiskColor,
+      whiteDiskColor: whiteDiskColor ?? this.whiteDiskColor,
       boardImage: boardImage ?? this.boardImage,
     );
   }
